fd0ee34da65518cb3cc5ace45ff380ca73cfc23a
SceptreOfficial <invictix@gmail.com>
2022-11-28 23:18:25 -0700

    Add vanilla keybinding conflict warning

--- addons/main/keybinding.hpp
+++ addons/main/keybinding.hpp
@@ -1,42 +1,42 @@
 class CfgUserActions {
 	class GVAR(action) {
 		displayName = CSTRING(Keybind_AIOAction);
-		tooltip = "";
+		tooltip = CSTRING(keybindWarning);
 		onActivate = QUOTE(call KEY_CODE(action));
 		onDeactivate = "";
 		onAnalog = "";
 	};
 	class GVAR(climb) {
 		displayName = CSTRING(Keybind_ClimbDropOnly);
-		tooltip = "";
+		tooltip = CSTRING(keybindWarning);
 		onActivate = QUOTE(call KEY_CODE(climb));
 		onDeactivate = "";
 		onAnalog = "";
 	};
 	class GVAR(jump) {
 		displayName = CSTRING(Keybind_JumpOnly);
-		tooltip = "";
+		tooltip = CSTRING(keybindWarning);
 		onActivate = QUOTE(call KEY_CODE(jump));
 		onDeactivate = "";
 		onAnalog = "";
 	};
 	class GVAR(assist) {
 		displayName = CSTRING(Keybind_Assist);
-		tooltip = "";
+		tooltip = CSTRING(keybindWarning);
 		onActivate = QUOTE(call KEY_CODE(assist));
 		onDeactivate = "";
 		onAnalog = "";
 	};
 	class GVAR(holster) {
 		displayName = CSTRING(Keybind_HolsterWeapon);
-		tooltip = "";
+		tooltip = CSTRING(keybindWarning);
 		onActivate = QUOTE(call KEY_CODE(holster));
 		onDeactivate = "";
 		onAnalog = "";
 	};
 	class GVAR(interact) {
 		displayName = CSTRING(Keybind_Interact);
-		tooltip = "";
+		tooltip = CSTRING(keybindWarning);
 		onActivate = QUOTE(call KEY_CODE(interact));
 		onDeactivate = "";
 		onAnalog = "";
